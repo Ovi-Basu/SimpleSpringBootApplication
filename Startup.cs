a05b850d14a20f24303601bf8e871d4e2d8a03fd
Atin Kumar Basu <47062819+Ovi-Basu@users.noreply.github.com>
2019-01-27 22:40:01 +0600

    Simple Spring Boot Application
    ASP.NET MVC Project

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimpleSpringBootApplication.Startup))]
+namespace SimpleSpringBootApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
